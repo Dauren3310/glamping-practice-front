264aa418587470910920684767ecc4c9fc8433f5
Igor <zahik1311@gmail.com>
2021-06-19 17:56:13 +0600

    #1 some new trests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+asd;
 
 function App() {
-  return (
-    <div className="App">
-      App for experiments
-    </div>
-  );
+  return <div className='App'>App for experiments</div>;
 }
 
 export default App;
